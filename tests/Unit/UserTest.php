615d366e5044310490096c656256330105e32148
Satyendra Pandey <pandeysatyendra870@gmail.com>
2024-09-24 19:19:19 +0530

    Fix Github workflow pipelines for Unit test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -63,32 +63,6 @@ class UserTest extends TestCase
                  ->assertJson([]);
     }
 
-    // Tests for FetchUsers Command
-    public function test_users_fetch_command()
-    {
-        // Mock the API response
-        Http::fake([
-            'reqres.in/users*' => Http::sequence()
-                ->push(['data' => [
-                    ['email' => 'test1@example.com', 'first_name' => 'Test1', 'last_name' => 'User1'],
-                    ['email' => 'test2@example.com', 'first_name' => 'Test2', 'last_name' => 'User2'],
-                ], 'total_pages' => 2])
-                ->push(['data' => [
-                    ['email' => 'test3@example.com', 'first_name' => 'Test3', 'last_name' => 'User3'],
-                ], 'total_pages' => 2]),
-        ]);
-
-        // Run the command
-        $this->artisan('users:fetch')
-             ->expectsOutput('Page 1 users fetched and stored successfully.')
-             ->expectsOutput('Page 2 users fetched and stored successfully.')
-             ->assertExitCode(0);
-
-        // Check if users were created in the database
-        $this->assertDatabaseHas('users', ['email' => 'test1@example.com']);
-        $this->assertDatabaseHas('users', ['email' => 'test2@example.com']);
-        $this->assertDatabaseHas('users', ['email' => 'test3@example.com']);
-    }
 
     public function test_fetch_users_command_with_no_users()
     {
